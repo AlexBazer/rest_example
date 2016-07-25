85512fe2543a5277148650b3310402f0758ac5c6
Alexandr Balagura <balagura@steelkiwi.com>
2016-07-25 17:09:43 +0300

    Create related name for task parent field

--- rest/urls.py
+++ rest/urls.py
@@ -21,6 +21,7 @@ import users.urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(tasks.urls)),
     url(r'^', include(users.urls)),
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -5,4 +5,4 @@ class TaskModel(models.Model):
     title = models.CharField('Title', max_length=255)
     desctiption = models.TextField('Description')
     created = models.DateTimeField('Created', auto_now_add=True)
-    parent = models.ForeignKey('self', null=True, default=None)
+    parent = models.ForeignKey('self', null=True, default=None, related_name='sub_tasks')
