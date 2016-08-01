042e462315716a0b2f116c6b3dbd23473a97dd68
Alexandr Balagura <balagura@steelkiwi.com>
2016-08-01 10:57:46 +0300

    Add test for total task/sub_tasks update

--- tasks/factories.py
+++ tasks/factories.py
@@ -3,18 +3,9 @@ from .models import TaskModel
 import factory
 
 
-class ParentSingleTaskFactory(factory.Factory):
+class TaskFactory(factory.Factory):
     class Meta:
         model = TaskModel
 
-    title = factory.Faker('text', length=50)
+    title = factory.Faker('text', max_nb_chars=10)
     description = factory.Faker('text')
-
-
-# class ParentWithSubsTaskFactory(factory.Factory):
-#     class Meta:
-#         model = TaskModel
-
-#     title = factory.Faker('text', length=50)
-#     description = factory.Faker('text')
-#     sub_tasks = factory.Iterator()
--- tasks/tests.py
+++ tasks/tests.py
@@ -2,7 +2,8 @@ from rest_framework import test, status
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 from .models import TaskModel
-from .factories import ParentSingleTaskFactory
+from .factories import TaskFactory
+from factory import fuzzy
 
 User = get_user_model()
 
@@ -17,6 +18,10 @@ class TaskCreateUpdateTestCase(test.APITestCase):
             username=self.user.username,
             password=self.user_password
         )
+        self.parent_task = TaskFactory()
+        self.parent_task.save()
+        self.children_tasks = TaskFactory.create_batch(3, parent=self.parent_task)
+        [item.save() for item in self.children_tasks]
 
     def test_task_create_with_nested(self):
         """ Create new note with nested tasks as children """
@@ -41,5 +46,40 @@ class TaskCreateUpdateTestCase(test.APITestCase):
         self.assertEqual(created_children.count(), 2)
 
     def test_task_update_with_nested(self):
-        """ Update parent task fields """
-        pass
+        """ Update parent task fields and childeren task fields
+
+        sub_tasks will include all earlier created children ids with new title each
+        """
+        new_children_tasks = [
+            {
+                'id': item.id,
+                'title': fuzzy.FuzzyText().fuzz(),
+                'description': item.description
+            } for item in self.children_tasks
+        ]
+
+        new_parent_task = {
+            'title': fuzzy.FuzzyText().fuzz(),
+            'description': self.parent_task.description
+        }
+        new_parent_task['sub_tasks'] = new_children_tasks
+        response = self.client.put(
+            reverse('taskmodel-detail', kwargs={'pk': self.parent_task.id}),
+            new_parent_task,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Get object from db
+        parent_task = TaskModel.objects.get(id=self.parent_task.id)
+        children_tasks = TaskModel.objects.filter(
+            id__in=[item.id for item in self.children_tasks]
+        )
+        # Compare db parent task title and new one
+        self.assertEqual(parent_task.title, new_parent_task['title'])
+
+        # Compare db subtasks title with new ones
+        [
+            self.assertEqual(item[1].title, item[0]['title'])
+            for item in zip(new_children_tasks, children_tasks)
+        ]
